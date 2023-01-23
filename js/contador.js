b9c5c133809a6c5c5556353f78b9a5395861e516
Samuel Vasquez <samivazqueles@gmail.com>
2023-01-23 12:39:48 -0500

    v2

---
+++ js/contador.js
@@ -0,0 +1,298 @@
+var contador = document.querySelector('#contador');
+
+
+//Botones $Valor del producto
+    //1.5M 1X1 TM
+const unoauno = document.querySelector('#unoauno');
+    //1.5M HDMI TM
+const hdmi = document.querySelector('#hdmi');    
+    //1.5M HDMI TM
+const yda6 = document.querySelector('#yda6');    
+    //TM 8GB USB
+const usb8 = document.querySelector('#usb8');    
+    //YD-ST2
+const YDST2 = document.querySelector('#YD-ST2');    
+    //JR1
+const JR1 = document.querySelector('#JR1');    
+    //P39
+const P39 = document.querySelector('#P39');    
+    //TWS
+const TWS = document.querySelector('#TWS');    
+    //H5
+const H5 = document.querySelector('#H5');    
+    //Z1
+const Z1 = document.querySelector('#Z1');    
+    //Y68
+const Y68 = document.querySelector('#Y68');    
+    //MUSB
+const MUSB = document.querySelector('#MUSB');    
+    //GPAD
+const GPAD = document.querySelector('#GPAD');    
+    //SD8
+const SD8 = document.querySelector('#SD8');    
+    //SD8
+const G100 = document.querySelector('#G100');    
+
+
+
+//Botones ¡Me salio!
+    //1.5M 1X1 TM
+const buttonunoauno = document.querySelector('#unoaunoboton');
+    //1.5M HDMI TM
+const buttonhdmi = document.querySelector('#hdmiboton');
+    //YD-A6
+const buttonyda6 = document.querySelector('#yda6boton');
+    //TM 8GB USB
+const buttonusb8 = document.querySelector('#botonusb8');
+    //YD-ST2
+const buttonYDST2 = document.querySelector('#botonYDST2');
+    //JR1
+const buttonJR1 = document.querySelector('#botonJR1');
+    //P39
+const buttonP39 = document.querySelector('#botonP39');
+    //TWS
+const buttonTWS = document.querySelector('#botonTWS');
+    //H5
+const buttonH5 = document.querySelector('#botonH5');
+    //H5
+const buttonZ1 = document.querySelector('#botonZ1');
+    //Y68
+const buttonY68 = document.querySelector('#botonY68');
+    //MUSB
+const buttonMUSB = document.querySelector('#botonMUSB');
+    //GPAD
+const buttonGPAD = document.querySelector('#botonGPAD');
+    //SD8
+const buttonSD8 = document.querySelector('#botonSD8');
+    //G100
+const buttonG100 = document.querySelector('#botonG100');
+
+
+
+
+contador.addEventListener('click', () => {
+    contador.innerHTML = 0
+})
+
+
+contador.innerHTML = 0
+
+buttonunoauno.addEventListener('click', () => {
+    var numero = unoauno.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+
+buttonhdmi.addEventListener('click', () => {
+    var numero = hdmi.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonyda6.addEventListener('click', () => {
+    var numero = yda6.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonusb8.addEventListener('click', () => {
+    var numero = usb8.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonYDST2.addEventListener('click', () => {
+    var numero = YDST2.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonJR1.addEventListener('click', () => {
+    var numero = JR1.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonP39.addEventListener('click', () => {
+    var numero = P39.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonTWS.addEventListener('click', () => {
+    var numero = TWS.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonH5.addEventListener('click', () => {
+    var numero = H5.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonZ1.addEventListener('click', () => {
+    var numero = Z1.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonY68.addEventListener('click', () => {
+    var numero = Y68.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonMUSB.addEventListener('click', () => {
+    var numero = MUSB.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonGPAD.addEventListener('click', () => {
+    var numero = GPAD.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonSD8.addEventListener('click', () => {
+    var numero = SD8.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+
+buttonG100.addEventListener('click', () => {
+    var numero = G100.innerHTML 
+    const valor = Number(numero)
+
+    var suma = contador.innerHTML
+
+    var final = Number(contador.innerHTML) + valor
+
+    contador.innerHTML = final
+
+    console.log(final) 
+})
+
+ 
+
+
+
+
+
+
+
+ 
